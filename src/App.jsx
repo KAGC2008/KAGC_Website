7595bc66d9e167e055c2f39cac285eedd68cef81
AUSTINJR74 <astnjs74@gmail.com>
2025-05-18 19:31:59 +0530

    ic

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-row gap-4">
+      <p>kgfdg</p>
+      <p>dfgsdg</p>
+      <p>sdgfsd</p>
+    </div>
+  );
+}
+
+export default App;
